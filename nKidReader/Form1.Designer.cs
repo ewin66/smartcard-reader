aa5cf2a8d0d3a11165222db53cbcafdf3d5f3d6c
nhatnguyennigellus <brightsun_nigellus4492@yahoo.com.vn>
2014-09-23 15:38:04 +0700

    Modify project UI & Icon
    - Main Form UI as described
    - Form icon & notifyReader icon

--- nKidReader/Form1.Designer.cs
+++ nKidReader/Form1.Designer.cs
@@ -37,8 +37,13 @@
             this.pbCameraPreview = new System.Windows.Forms.PictureBox();
             this.cbWcOn = new System.Windows.Forms.CheckBox();
             this.cbCamList = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.contextMenuStripMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbCameraPreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // notifyReader
@@ -74,7 +79,7 @@
             // 
             // pbCameraPreview
             // 
-            this.pbCameraPreview.Location = new System.Drawing.Point(29, 24);
+            this.pbCameraPreview.Location = new System.Drawing.Point(376, 50);
             this.pbCameraPreview.Name = "pbCameraPreview";
             this.pbCameraPreview.Size = new System.Drawing.Size(320, 240);
             this.pbCameraPreview.TabIndex = 1;
@@ -83,7 +88,7 @@
             // cbWcOn
             // 
             this.cbWcOn.AutoSize = true;
-            this.cbWcOn.Location = new System.Drawing.Point(395, 44);
+            this.cbWcOn.Location = new System.Drawing.Point(388, 16);
             this.cbWcOn.Name = "cbWcOn";
             this.cbWcOn.Size = new System.Drawing.Size(109, 17);
             this.cbWcOn.TabIndex = 2;
@@ -95,17 +100,65 @@
             // 
             this.cbCamList.Enabled = false;
             this.cbCamList.FormattingEnabled = true;
-            this.cbCamList.Location = new System.Drawing.Point(391, 75);
+            this.cbCamList.Location = new System.Drawing.Point(549, 14);
             this.cbCamList.Name = "cbCamList";
             this.cbCamList.Size = new System.Drawing.Size(121, 21);
             this.cbCamList.TabIndex = 3;
             this.cbCamList.SelectedIndexChanged += new System.EventHandler(this.cbCamList_SelectedIndexChanged);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::nKidReader.Properties.Resources.nkid;
+            this.pictureBox1.Location = new System.Drawing.Point(121, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 182);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(63, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Proximity Reader by NKID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(111, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "2014 © NKID JSC.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(119, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "All Rights Reserved";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 452);
+            this.ClientSize = new System.Drawing.Size(732, 315);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.cbCamList);
             this.Controls.Add(this.cbWcOn);
             this.Controls.Add(this.pbCameraPreview);
@@ -116,6 +169,7 @@
             this.Resize += new System.EventHandler(this.MainForm_Resize);
             this.contextMenuStripMain.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbCameraPreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,6 +184,10 @@
         private System.Windows.Forms.PictureBox pbCameraPreview;
         private System.Windows.Forms.CheckBox cbWcOn;
         private System.Windows.Forms.ComboBox cbCamList;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
